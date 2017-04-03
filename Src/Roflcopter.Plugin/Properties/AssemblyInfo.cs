1310bfb317f3d5ac64d930ef62cb70c879b73286
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-04-04 01:54:00 +0200

    Employ new (MSBuild based) .NET Core projects (with .NET 4.5 TargetFW) and update to latest R# SDK

--- Src/Roflcopter.Plugin/Properties/AssemblyInfo.cs
+++ Src/Roflcopter.Plugin/Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle(AssemblyConsts.Title)]
+//
+
 [assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1.0-dev")]
 
 // ReSharper disable once CheckNamespace
 internal static class AssemblyConsts
