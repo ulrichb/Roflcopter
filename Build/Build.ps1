f14c75fc07e46df0406a656f86702b66bc889f12
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-08-02 01:54:02 +0200

    Add (empty) test project

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -3,8 +3,8 @@ Param(
   [Parameter()] [string] $NugetExecutable = "Shared\.nuget\nuget.exe",
   [Parameter()] [string] $Configuration = "Debug",
   [Parameter()] [string] $Version = "0.0.0.1-local",
-#  [Parameter()] [string] $BranchName,
-#  [Parameter()] [string] $CoverageBadgeUploadToken,
+  [Parameter()] [string] $BranchName,
+  [Parameter()] [string] $CoverageBadgeUploadToken,
   [Parameter()] [string] $NugetPushKey
 )
 
@@ -16,12 +16,12 @@ $BuildOutputPath = "Build\Output"
 $SolutionFilePath = "Roflcopter.sln"
 $AssemblyVersionFilePath = "Src\Roflcopter.Plugin\Properties\AssemblyInfo.cs"
 $MSBuildPath = (Get-ChildItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\*\MSBuild\15.0\Bin\MSBuild.exe").FullName
-# $NUnitExecutable = "nunit-console-x86.exe"
-# $NUnitTestAssemblyPaths = @(
-#   "Src\Roflcopter.Plugin.Tests\bin.R82\$Configuration\Roflcopter.Plugin.Tests.dll"
-# )
-# $NUnitFrameworkVersion = "net-4.5"
-# $TestCoverageFilter = "+[Roflcopter*]* -[Roflcopter*]ReSharperExtensionsShared.*"
+$NUnitAdditionalArgs = "--x86 --labels=All --agents=1"
+$NUnitTestAssemblyPaths = @(
+    "Src\Roflcopter.Plugin.Tests\bin\$Configuration\Roflcopter.Plugin.Tests.dll"
+)
+$NUnitFrameworkVersion = "net-4.5"
+$TestCoverageFilter = "+[Roflcopter*]* -[Roflcopter*]ReSharperExtensionsShared.*"
 $NuspecPath = "Src\Roflcopter.nuspec"
 $NugetPackProperties = @(
     "Version=$(CalcNuGetPackageVersion 20171);Configuration=$Configuration;DependencyVer=[8.0];BinDirInclude=bin"
@@ -32,7 +32,7 @@ Clean
 PackageRestore
 Build
 NugetPack
-# Test
+Test
 
 if ($NugetPushKey) {
     NugetPush
---
+++ Src/Roflcopter.Plugin.Tests/ZoneMarkerAndSetUpFixture.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+using Roflcopter.Plugin.Tests;
+
+[assembly: RequiresSTA]
+
+namespace Roflcopter.Plugin.Tests
+{
+    [ZoneDefinition]
+    public interface IRoflcopterTestEnvironmentZone : ITestsZone, IRequire<PsiFeatureTestZone>
+    {
+    }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<IRoflcopterTestEnvironmentZone>
+    {
+    }
+}
+
+[SetUpFixture]
+public class TestEnvironmentSetUpFixture : ExtensionTestEnvironmentAssembly<IRoflcopterTestEnvironmentZone>
+{
+}
