b1138d5e8d84a8ba70b9773624b677f0f92c8850
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-11-13 00:57:19 +0100

    Add missing ZoneMarker

---
+++ Src/Roflcopter.Plugin/UnitTesting/ZoneMarker.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.UnitTestFramework;
+
+namespace Roflcopter.Plugin.UnitTesting
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<IUnitTestingZone>
+    {
+    }
+}
