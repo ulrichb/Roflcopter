471687046507a4089d568807954288608465c04f
Ulrich Buchgraber <ulrich.b@gmx.at>
2020-01-31 19:58:28 +0100

    Fix ZoneMarker for Debugging Control feature to avoid exceptions in InspectCode

--- Src/Roflcopter.Plugin/VisualStudio/ModulesWindow/ZoneMarker.cs
+++ Src/Roflcopter.Plugin/VisualStudio/ModulesWindow/ZoneMarker.cs
@@ -1,12 +1,12 @@
 #if RESHARPER
 using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.Platform.VisualStudio.SinceVs10.Shell.Zones;
-using JetBrains.ReSharper.Resources.Shell;
+using JetBrains.ReSharper.Feature.Services.DebuggerVs;
 
 namespace Roflcopter.Plugin.VisualStudio.ModulesWindow
 {
     [ZoneMarker]
-    public class ZoneMarker : IRequire<ISinceVs10EnvZone>, IRequire<PsiFeaturesImplZone>
+    public class ZoneMarker : IRequire<ISinceVs10EnvZone>, IRequire<IVsDebuggerZone>
     {
     }
 }
