dec742af4176149ab7dd80d747c516d9c0ca9903
Ulrich Buchgraber <ulrich.b@gmx.at>
2023-01-08 20:15:43 +0100

    Move to resource strings

--- Src/Roflcopter.Plugin/Macros/GetBranchNameMacroDefinition.cs
+++ Src/Roflcopter.Plugin/Macros/GetBranchNameMacroDefinition.cs
@@ -3,12 +3,12 @@ using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
 
 namespace Roflcopter.Plugin.Macros
 {
-    [MacroDefinition(Name, ShortDescription = ShortDescription, LongDescription = LongDescription)]
+    [MacroDefinition(Name, ResourceType = typeof(Strings),
+        DescriptionResourceName = nameof(Strings.GetBranchNameMacro_Description),
+        LongDescriptionResourceName = nameof(Strings.GetBranchNameMacro_LongDescription))]
     public class GetBranchNameMacroDefinition : SimpleMacroDefinition
     {
         private const string Name = "getBranchName";
-        private const string ShortDescription = "Current branch name, stripped by {#0:an optional regex}";
-        private const string LongDescription = "Returns the current Git branch name (using Git's HEAD file)";
 
         [ExcludeFromCodeCoverage /* just a declaration, tested manually */]
         public override ParameterInfo[] Parameters => new[] { new ParameterInfo(ParameterType.String) };
