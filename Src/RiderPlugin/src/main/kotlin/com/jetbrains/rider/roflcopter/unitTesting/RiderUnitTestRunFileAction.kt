e9fb4462faa6c17b15898f84fbef15e0fec24882
Ulrich Buchgraber <ulrich.b@gmx.at>
2024-09-03 18:57:59 +0200

    Use default (type generated) ActionID and move string to resource in `UnitTestRunFileAction`

--- Src/RiderPlugin/src/main/kotlin/com/jetbrains/rider/roflcopter/unitTesting/RiderUnitTestRunFileAction.kt
+++ Src/RiderPlugin/src/main/kotlin/com/jetbrains/rider/roflcopter/unitTesting/RiderUnitTestRunFileAction.kt
@@ -2,4 +2,4 @@ package com.jetbrains.rider.roflcopter.unitTesting;
 
 import com.jetbrains.rider.unitTesting.actions.base.RiderUnitTestAnActionBase
 
-class RiderUnitTestRunFileAction : RiderUnitTestAnActionBase("UnitTest.RunFile")
+class RiderUnitTestRunFileAction : RiderUnitTestAnActionBase("UnitTestRunFile")
